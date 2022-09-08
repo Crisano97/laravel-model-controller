eddda7e23b1add0e376275e58cb54ec3377ebde4
Crisano97 <97keb6@gmail.com>
2022-09-08 15:20:00 +0200

    obtaining films datas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,17 @@
 @extends('layout.main')
 
 @section('main-content')
-@dump('movies')
     @forelse ($movies as $movie)
-        <h1>{{ $movie->title }}</h1>
+        <div class="card">
+            <h1>{{ $movie->title }}</h1>
+            <h2>{{ $movie->original_title }}</h2>
+            <span>{{ $movie->nationality }}</span>
+            <span>{{ $movie->date }}</span>
+            <span>{{ $movie->vote }}</span>
+        </div>
         
     @empty
         <p>no movies foud</p>
     @endforelse
+    
 @endsection
