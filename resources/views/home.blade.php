00ccbff71236db3d7c9d7d4a79b7f748e290b054
Crisano97 <97keb6@gmail.com>
2022-09-08 15:31:41 +0200

    showing movies cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,21 @@
 @extends('layout.main')
 
 @section('main-content')
-    @forelse ($movies as $movie)
-        <div class="card">
-            <h1>{{ $movie->title }}</h1>
-            <h2>{{ $movie->original_title }}</h2>
-            <span>{{ $movie->nationality }}</span>
-            <span>{{ $movie->date }}</span>
-            <span>{{ $movie->vote }}</span>
-        </div>
-        
-    @empty
-        <p>no movies foud</p>
-    @endforelse
-    
+    <div class="container my-4">
+        <div class="row justify-content-between">
+        @forelse ($movies as $movie)
+                <div class="card col-2 mx-3 my-4 border border-5">
+                    <h1>{{ $movie->title }}</h1>
+                    <h2>{{ $movie->original_title }}</h2>
+                    <span>{{ $movie->nationality }}</span>
+                    <span>{{ $movie->date }}</span>
+                    <span>{{ $movie->vote }}</span>
+                </div>
+                
+                @empty
+                <p>no movies foud</p>
+                @endforelse
+                
+            </div>
+    </div>
 @endsection
