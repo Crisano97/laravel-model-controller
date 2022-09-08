1a18313600cb6f7fcdee8a4f01da93d017585397
Crisano97 <97keb6@gmail.com>
2022-09-08 15:37:43 +0200

    adjustments on home blade indentation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,19 @@
 @section('main-content')
     <div class="container my-4">
         <div class="row justify-content-between">
-        @forelse ($movies as $movie)
+            @forelse ($movies as $movie)
                 <div class="card col-2 mx-3 my-4 border border-5">
-                    <h1>{{ $movie->title }}</h1>
-                    <h2>{{ $movie->original_title }}</h2>
+                    <h3>{{ $movie->title }}</h3>
+                    <h5>{{ $movie->original_title }}</h5>
                     <span>{{ $movie->nationality }}</span>
                     <span>{{ $movie->date }}</span>
                     <span>{{ $movie->vote }}</span>
                 </div>
                 
-                @empty
+            @empty
                 <p>no movies foud</p>
-                @endforelse
+            @endforelse
                 
-            </div>
+        </div>
     </div>
 @endsection
